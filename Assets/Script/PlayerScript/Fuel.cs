86703067bb29123568109ee332f939cde289e283
SeungJu0406 <nss8164@gmail.com>
2024-08-24 23:04:43 +0900

    feat : 연료 바 추가

--- Assets/Script/PlayerScript/Fuel.cs
+++ Assets/Script/PlayerScript/Fuel.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Fuel : MonoBehaviour
@@ -19,14 +17,14 @@ public class Fuel : MonoBehaviour
 
     void StartInit()
     {
-        rotateWings= wing.GetComponent<RotateWings>();
+        rotateWings = wing.GetComponent<RotateWings>();
     }
     void PrintFuel()
     {
-        if (rotateWings.fuel > 0) 
+        if (rotateWings.fuel > 0)
         {
             transform.localScale = new Vector3(
-                transform.localScale.x - ( 100 - rotateWings.fuel) ,
+                1 - (1.0f - rotateWings.fuel / 100.0f),
                 transform.localScale.y,
                 transform.localScale.z);
         }
