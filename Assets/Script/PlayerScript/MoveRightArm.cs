726ac0eb9bfbea88ae0a71626537590b452f9d26
SeungJu0406 <nss8164@gmail.com>
2024-08-25 21:20:40 +0900

    feat : 도라에몽 팔 피봇 위치 변경

--- Assets/Script/PlayerScript/MoveRightArm.cs
+++ Assets/Script/PlayerScript/MoveRightArm.cs
@@ -12,32 +12,18 @@ public class MoveRightArm : MonoBehaviour
     private Direction nowDir;
     private void Update()
     {
-        Check();
+        //Check();
         Rotate();
         Debug.Log($"{nowDir}");
         Debug.Log($"{transform.rotation.z}");
     }
     void Rotate()
     {
-        if (nowDir == Direction.Up)
-        {
-            transform.RotateAround(target.position, Vector3.forward, -1* speed * Time.deltaTime);
-        }
-        else if (nowDir == Direction.Down)
-        {
-            transform.RotateAround(target.position, Vector3.forward, speed * Time.deltaTime);
-        }
+        transform.Rotate(Vector3.forward,speed*Time.deltaTime);
     }
 
     void Check()
     {
-        if(transform.rotation.z <= -185)
-        {
-            nowDir = Direction.Up;
-        }
-        else if (transform.rotation.z >= 100)
-        {
-            nowDir = Direction.Down;
-        }
+
     }
 }
