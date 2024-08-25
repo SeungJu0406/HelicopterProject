22cacdd50c67bc9ab7af991b93342be91798ce47
SeungJu0406 <nss8164@gmail.com>
2024-08-25 23:07:55 +0900

    feat : 태양계 자전 및 방향 설정 가능 추가

--- Assets/Script/SolarSystem/NewBehaviourScript.cs
+++ Assets/Script/SolarSystem/NewBehaviourScript.cs
@@ -2,14 +2,38 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField] float speed;
+    enum RotateDir { Left, Right }
+    [SerializeField] float rotateSpeed;
+    [SerializeField] RotateDir rotateDir;
+    enum OrbitRotateDir { Left, Right }
+    [SerializeField] float orbSpeed;
+    [SerializeField] OrbitRotateDir orbDir;
     [SerializeField] Transform target;
     private void Update()
     {
         Rotate();
+        OrvitRotate();
     }
     void Rotate()
     {
-        transform.RotateAround(target.position, Vector3.up, speed * Time.deltaTime);
+        if(rotateDir == RotateDir.Right)
+        {
+            transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+        else if(rotateDir == RotateDir.Left)
+        {
+            transform.Rotate(Vector3.down * rotateSpeed * Time.deltaTime);
+        }
+    }
+    void OrvitRotate()
+    {
+        if (orbDir == OrbitRotateDir.Right)
+        {
+            transform.RotateAround(target.position, Vector3.up, orbSpeed * Time.deltaTime);
+        }
+        else if (orbDir == OrbitRotateDir.Left)
+        {
+            transform.RotateAround(target.position, Vector3.down, orbSpeed * Time.deltaTime);
+        }
     }
 }
